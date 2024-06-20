240373c4259859c3d3289553e5e1ad6e0313c624
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-06-21 02:36:18 +0700

    games page checkpoint

--- dist/js/bunch-of-games.js
+++ dist/js/bunch-of-games.js
@@ -1,4 +1,5 @@
 const gameCollectionPlace = document.querySelectorAll('.gameCollection');
+const listPlace = document.querySelectorAll('#gameList');
 const gameCollection1 = [
     {
         img: "../assets/resources/img/chemistry.png",
@@ -51,6 +52,59 @@ const gameCollection1 = [
         reward: "10",  
     },
 ];
+const gameCollection2 = [
+    {   
+        img: "../assets/resources/img/nation.png",
+        title: "Nation Guess",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/thumb.png",
+        title: "Thumb War",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/bomb.png",
+        title: "Bomb It",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/2k-shoot.png",
+        title: "2k shoot",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/brick-breaker.png",
+        title: "Brick Breaker",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/parkour.png",
+        title: "Parkour Race",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/trackto.png",
+        title: "Tracktor Off-road",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/mad.png",
+        title: "Mad King",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/badminton.png",
+        title: "Badmin League",
+        reward: "10",  
+    },
+    {
+        img: "../assets/resources/img/superfire.png",
+        title: "Mr. Superfire",
+        reward: "10",  
+    },
+];
+
 
 function displayGamesCollection1(container, games) {
     games.forEach(element => {
@@ -67,4 +121,8 @@ function displayGamesCollection1(container, games) {
 
 gameCollectionPlace.forEach(element => {
     displayGamesCollection1(element, gameCollection1);
+});
+
+listPlace.forEach(element => {
+    displayGamesCollection1(element, gameCollection2);
 });
